e880b00219eb40043211a8def43079c96ad822df
Russ Whatley <russ.c.w@me.com>
2023-04-20 14:00:03 -0400

    Version 1.0 - Orange

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -1,26 +1,29 @@
-//
-//  ContentView.swift
-//  GitHub Exercise
-//
-//  Created by Russell Whatley on 4/20/23.
-//
+     //
+     //  ContentView.swift
+     //  GitHub Exercise
+     //
+     //  Created by Russell Whatley on 4/20/23.
+     //
 
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+     var body: some View {
+          VStack {
+               Image(systemName: "1.circle")
+                    .resizable()
+                    .scaledToFit()
+               Text("Version 1.0")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+          }
+          .padding()
+          .foregroundColor(.orange)
+     }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+     static var previews: some View {
+          ContentView()
+     }
 }
